045144fa2ea74f00b736dc95a258b1000c3eb953
Vini Bjazevich <vinibjazevich97@gmail.com>
2021-03-10 14:01:16 -0800

    Successfully askes user if they are willing to enable location settings. If so gets the users current long and lat and prints it to the screen

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    getLocation();
+  }, [])
+
+  async function getLocation() {
+    try {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {location ? <Text>{`Lat: ${location.coords.latitude}, Long: ${location.coords.longitude}`}</Text> : <Text>You need to enable location for this app</Text>}
       <StatusBar style="auto" />
     </View>
   );
