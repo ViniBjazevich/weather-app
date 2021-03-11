435194e1f4be8589c3341fb749b2262d03f266a5
Vini Bjazevich <vinibjazevich97@gmail.com>
2021-03-10 20:39:01 -0800

    Created components for the top, middle, and bottom of app

---
+++ components/WeatherInfo.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { View, Text, Image, StyleSheet, ActivityIndicator} from 'react-native'
+
+export default function WeatherInfo(props) {
+  if (props.currentWeather && props.location) {
+    let img = props.currentWeather.weather[0].icon;
+    let city = props.currentWeather.name;
+    let description = props.currentWeather.weather[0].description;
+    let temp = Math.floor(props.currentWeather.main.temp);
+    return (
+      <View style={styles.container}>
+        <Text style={styles.city}>{city}</Text>
+        <Text style={styles.description}>{`${description}`}</Text>
+        <Image source={{uri: `https://openweathermap.org/img/wn/${img}@4x.png`}} style={styles.image}/>
+        <Text style={styles.temp}>{`${temp} °F`}</Text>
+      </View>
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator />
+      </View>
+    )
+  }
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    width: '100%',
+    backgroundColor: 'green',
+    flex: 3
+  },
+  image: {
+    height: 200,
+    width: 200,
+  },
+  city: {
+    fontSize: 30
+  },
+  temp: {
+    fontSize: 40
+  },
+  description: {
+    fontSize: 25
+  }
+
+});
+
